46ea172154ca7977f3731b4d3db80902343824dc
vicoslo <vicoslo@gmail.com>
2022-04-11 07:15:07 -0500

    plantilla

---
+++ database/migrations/2022_04_09_211044_create_despachos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDespachosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('despachos', function (Blueprint $table) {
+            $table->id();
+            $table->date ('fecha');
+            $table->integer('orden');
+            $table->integer('expedientes');
+            $table->integer('copias');
+            $table->integer('errores');
+            $table->integer('otros');
+            $table->decimal('total', $precision = 8, $scale = 2);
+
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('despachos');
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+@extends('adminlte::page')
+@section('content')
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
